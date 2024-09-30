05266b43b995550c91767ec6b81ddc86060cb945
Lucas Reis <lucasreisribeiro97@gmail.com>
2024-09-30 14:58:51 -0300

    feat: :sparkles: using DDD on Category and creating CategoryID

---
+++ domain/src/main/java/dev/reislucaz/catalogo/domain/category/CategoryID.java
@@ -0,0 +1,39 @@
+package dev.reislucaz.catalogo.domain.category;
+
+import dev.reislucaz.catalogo.domain.Identifier;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CategoryID extends Identifier {
+    private final String value;
+
+    private CategoryID(String value) {
+        this.value = value;
+    }
+
+    public static CategoryID unique() {
+        return new CategoryID(UUID.randomUUID().toString().toLowerCase());
+    }
+
+    public static CategoryID from(final String anId) {
+        return new CategoryID(anId);
+    }
+
+    public static CategoryID from(final UUID anId) {
+        return new CategoryID(anId.toString().toLowerCase());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoryID that = (CategoryID) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
