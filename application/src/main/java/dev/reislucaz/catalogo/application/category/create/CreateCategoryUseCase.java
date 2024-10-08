4b8a80059d0793794e0218a69716f9caf3fbf4a4
Lucas Reis <lucasreisribeiro97@gmail.com>
2024-10-08 16:59:26 -0300

    refactor: :recycle: using either pattern to return notification handler instead of throwing exception

--- application/src/main/java/dev/reislucaz/catalogo/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/dev/reislucaz/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,6 +1,8 @@
 package dev.reislucaz.catalogo.application.category.create;
 
 import dev.reislucaz.catalogo.application.UseCase;
+import dev.reislucaz.catalogo.domain.validation.handler.NotificationHandler;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, Either<NotificationHandler, CreateCategoryOutput>> {
 }
--- application/src/main/java/dev/reislucaz/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/dev/reislucaz/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
@@ -2,7 +2,9 @@ package dev.reislucaz.catalogo.application.category.create;
 
 import dev.reislucaz.catalogo.domain.category.Category;
 import dev.reislucaz.catalogo.domain.category.CategoryGateway;
+import dev.reislucaz.catalogo.domain.validation.handler.NotificationHandler;
 import dev.reislucaz.catalogo.domain.validation.handler.ThrowsValidationHandler;
+import io.vavr.control.Either;
 
 import java.util.Objects;
 
@@ -14,15 +16,21 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
     }
 
     @Override
-    public CreateCategoryOutput execute(CreateCategoryCommand aCommand) {
+    public Either<NotificationHandler, CreateCategoryOutput> execute(CreateCategoryCommand aCommand) {
         final var aName = aCommand.name();
         final var aDescription = aCommand.description();
         final var isActive = aCommand.isActive();
 
+        final var notification = NotificationHandler.create();
+
         final var aCategory = Category.newCategory(aName, aDescription, isActive);
 
-        aCategory.validate(new ThrowsValidationHandler());
+        aCategory.validate(notification);
 
-        return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
+        if (notification.hasError()) {
+            return Either.left(notification);
+        }
+
+        return Either.right(CreateCategoryOutput.from(this.categoryGateway.create(aCategory)));
     }
 }
