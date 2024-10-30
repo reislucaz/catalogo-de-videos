ec92839f5c07e20c4d89cf5f20d897d6bbaf078b
Lucas Reis <lucasreisribeiro97@gmail.com>
2024-10-30 15:20:55 -0300

    feat: :sparkles: find all categories paginated and filtered method implemented

--- infrastructure/src/main/java/dev/reislucaz/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/dev/reislucaz/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -7,8 +7,15 @@ import dev.reislucaz.catalogo.domain.pagination.Pagination;
 import dev.reislucaz.catalogo.domain.pagination.SearchQuery;
 import dev.reislucaz.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import dev.reislucaz.catalogo.infrastructure.category.persistence.CategoryRepository;
+import dev.reislucaz.catalogo.infrastructure.utils.SpecificationUtils;
+import static dev.reislucaz.catalogo.infrastructure.utils.SpecificationUtils.like;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.*;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -50,6 +57,22 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Pagination<Category> findAll(SearchQuery aQuery) {
-        return null;
+        final var page = PageRequest.of(aQuery.page(), aQuery.perPage(), Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort()));
+
+        final var specifications = Optional.ofNullable(aQuery.terms())
+                .filter(str -> str.isBlank())
+                .map(str ->
+                        SpecificationUtils.<CategoryJpaEntity>like("name", str)
+                        .or(like("description", str)))
+                .orElse(null);
+
+        final var pageResult = this.repository.findAll(Specification.where(specifications), page);
+
+        return new Pagination<Category>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(CategoryJpaEntity::toAggregate).toList()
+        );
     }
 }
